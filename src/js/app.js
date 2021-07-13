8a178251a78998522753988c89005e1fbf4c0ab1
kaorinS <pluvia.seed@icloud.com>
2021-07-13 18:48:54 +0900

    Model作成

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+var Backbone = require("backbone");
+var $ = require("jquery");
+var _ = require("underscore");
+
+// =====================================
+// Model
+// =====================================
+// タスク
+var ListItem = Backbone.Model.extend({
+  defaults: {
+    text: "",
+    isDone: false,
+    editMode: false,
+    isShow: true,
+  },
+});
+// TODO追加用フォーム
+var AddForm = Backbone.Model.extend({
+  defaults: {
+    val: "",
+    hasError: false,
+    errorMsg: "",
+  },
+});
+var addForm = new AddForm();
+// 検索用
+var Search = Backbone.Model.extend({
+  defaults: {
+    val: "",
+  },
+});
+var search = new Search();
